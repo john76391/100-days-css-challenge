e9da16553b2ae8e33e00056e6d6f44d0530630e8
John Liao <john76391@gmail.com>
2026-02-28 01:25:55 +0800

    complete day 2, might improve toggleMenu due to warning "Cannot access refs during render"

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import Home from "./pages/home";
 import Frame from "./pages/frame";
 import Day1 from "./pages/day1";
+import Day2 from "./pages/day2";
 
 import NotFound from "./pages/notFound";
 
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="days" element={<Frame />}>
           <Route index element={<Navigate to="1" />} />
           <Route path="1" element={<Day1 />} />
+          <Route path="2" element={<Day2 />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
---
+++ src/pages/day2.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP, type ContextSafeFunc } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+export default function Component() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const tl = useRef<gsap.core.Timeline | null>(null);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const { contextSafe } = useGSAP((context) => {
+    const yMove = 22;
+
+    if (!tl.current) {
+      tl.current = gsap.timeline({ paused: true, defaults: { duration: 0.3 } });
+    }
+
+    tl.current
+      .to("#line1", { y: yMove })
+      .to("#line2", { autoAlpha: 0, scale: 0 }, "<")
+      .to("#line3", { y: -yMove }, "<")
+      .to("#line1", {
+        rotation: 135,
+      })
+      .to(
+        "#line3",
+        {
+          rotation: 45,
+        },
+        "<",
+      );
+  });
+
+  const toggleMenu = contextSafe(() => {
+    setIsOpen(!isOpen);
+
+    if (!isOpen) {
+      tl.current.reverse();
+    } else {
+      tl.current.play();
+    }
+  });
+
+  return (
+    <div className="flex h-full items-center justify-center bg-[#3FAF82]">
+      <div
+        ref={containerRef}
+        onClick={toggleMenu}
+        className="flex h-13 w-20 cursor-pointer flex-col justify-between [--shadow-line:0_2px_10px_0_rgba(0,0,0,0.3)]"
+      >
+        <div
+          id="line1"
+          className="h-2 rounded-sm bg-white shadow-(--shadow-line)"
+        ></div>
+        <div
+          id="line2"
+          className="h-2 rounded-sm bg-white shadow-(--shadow-line)"
+        ></div>
+        <div
+          id="line3"
+          className="h-2 rounded-sm bg-white shadow-(--shadow-line)"
+        ></div>
+      </div>
+    </div>
+  );
+}
