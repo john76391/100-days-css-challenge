150093e1f77b963884068329fb745572f0b4d246
John Liao <john76391@gmail.com>
2026-03-01 22:03:11 +0800

    add day 4

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Frame from "./pages/frame";
 import Day1 from "./pages/day1";
 import Day2 from "./pages/day2";
 import Day3 from "./pages/day3";
+import Day4 from "./pages/day4";
+
 
 import NotFound from "./pages/notFound";
 
@@ -18,6 +20,7 @@ export default function App() {
           <Route path="1" element={<Day1 />} />
           <Route path="2" element={<Day2 />} />
           <Route path="3" element={<Day3 />} />
+          <Route path="4" element={<Day4 />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
