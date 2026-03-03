c7703317a0768815def79e40e36dc39bd9552a56
John Liao <john76391@gmail.com>
2026-03-03 13:24:11 +0800

    complete day 5

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Day1 from "./pages/day1";
 import Day2 from "./pages/day2";
 import Day3 from "./pages/day3";
 import Day4 from "./pages/day4";
+import Day5 from "./pages/day5";
+
 
 
 import NotFound from "./pages/notFound";
@@ -21,6 +23,7 @@ export default function App() {
           <Route path="2" element={<Day2 />} />
           <Route path="3" element={<Day3 />} />
           <Route path="4" element={<Day4 />} />
+          <Route path="5" element={<Day5 />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
